31acc913881db691d109adbbfcb517da683ee157
Hack Reactor Students <accounts@hackreactor.com>
2017-01-05 12:13:17 -0800

    implement SongQueue.js

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,11 +4,29 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-    console.log(this);
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      var firstModel = this.models.slice(0, 1);
+      this.remove(firstModel);
+
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function() {
+      var firstModel = this.models.slice(0, 1);
+      this.remove(firstModel);
+    });
   },
 
   playFirst: function() {
-
+    this.at(0).play();
   }
 
 });
