f953bd881edc88fefd3d79d40fdf5ee4fa849fae
Joey Lee <jleepianist@gmail.com>
2017-05-31 21:53:55 -0700

    (bug fix) Mistook enqueue for dequeue and added dequeue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,6 +3,7 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+
   initialize: function() {
     this.on('add', this.enqueue, this);
     this.on('dequeue', this.dequeue, this);
@@ -10,6 +11,12 @@ var SongQueue = Backbone.Collection.extend({
   },
 
   enqueue: function(song) {
+    if (this.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  dequeue: function(song) {
     if (this.at(0) === song) {
       this.playNext();
     } else {
@@ -29,5 +36,5 @@ var SongQueue = Backbone.Collection.extend({
   playFirst: function() {
     this.at(0).play();
   }
-
+  
 });
